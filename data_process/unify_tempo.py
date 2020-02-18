9b9eb51fbea10a943693da2c8682a7b84d68dd4f
truedick23 <dingzhx@vip.qq.com>
2020-02-18 14:17:13 +0800

    found ways to scale bpm

--- data_process/unify_tempo.py
+++ data_process/unify_tempo.py
@@ -1,7 +1,11 @@
 from pymongo import MongoClient
+import pretty_midi
+import os
 from pypianoroll import Multitrack
-from data_process.track_merge import get_merged
-import midi
+from pypianoroll import Multitrack, Track
+import mido
+import music21
+
 def get_midi_info(pm):
     """Return useful information from a pretty_midi.PrettyMIDI instance"""
     if pm.time_signature_changes:
@@ -30,28 +34,101 @@ def get_midi_info(pm):
 
     return midi_info
 
+TRACK_INFO = (
+    ('Drums', 0),
+    ('Piano', 0),
+    ('Guitar', 24),
+    ('Bass', 32),
+    ('Strings', 48),
+)
+
+def get_merged(multitrack):
+    """Merge the multitrack pianorolls into five instrument families and
+    return the resulting multitrack pianoroll object."""
+    track_lists_to_merge = [[] for _ in range(5)]
+    for idx, track in enumerate(multitrack.tracks):
+        if track.is_drum:
+            track_lists_to_merge[0].append(idx)
+        elif track.program//8 == 0:
+            track_lists_to_merge[1].append(idx)
+        elif track.program//8 == 3:
+            track_lists_to_merge[2].append(idx)
+        elif track.program//8 == 4:
+            track_lists_to_merge[3].append(idx)
+        elif track.program < 96 or 104 <= track.program < 112:
+            track_lists_to_merge[4].append(idx)
+
+    tracks = []
+    for idx, track_list_to_merge in enumerate(track_lists_to_merge):
+        if track_list_to_merge:
+            merged = multitrack[track_list_to_merge].get_merged_pianoroll('max')
+            tracks.append(Track(merged, TRACK_INFO[idx][1], (idx == 0),
+                                TRACK_INFO[idx][0]))
+        else:
+            tracks.append(Track(None, TRACK_INFO[idx][1], (idx == 0),
+                                TRACK_INFO[idx][0]))
+    return Multitrack(None, tracks, multitrack.tempo, multitrack.downbeat,
+                      multitrack.beat_resolution, multitrack.name)
 
 def get_midi_collection():
     client = MongoClient(connect=False)
     return client.free_midi.midi
 
+def get_tempo(path):
+    pm = pretty_midi.PrettyMIDI(path)
+    return get_midi_info(pm)['tempo']
 
-def tempo_unify():
+
+def merge_then_tempo_unify():
 
     midi_collection = get_midi_collection()
     root_dir = 'E:/free_midi_library/'
-    test_path = './test.mid'
-    unify_tempo = 90
-    original = Multitrack(test_path)
-    original_tempo = get_midi_info(original.to_pretty_midi())['tempo'][0]
-    merged_multi = get_merged(original)
-    tracks_dict = {}
-    length = 0
-    for track in merged_multi.tracks:
-        if track.pianoroll.shape[0] != 0:
-            length = track.pianoroll.shape[0]
-            old_pianoroll = track.pianoroll
-            for i in range(128):
-                for j in range(length):
-                    print(track.pianoroll[j][i], end=' ')
-                print()
+    merged_root_dir = 'E:/merged_midi/'
+
+    for midi in midi_collection.find({'MergedAndScaled': False}, no_cursor_timeout = True):
+        original_path = os.path.join(root_dir, midi['Genre'] + '/', midi['md5'] + '.mid')
+        original_tempo = get_tempo(original_path)[0]
+        changed_rate = original_tempo / 120
+
+        if not os.path.exists(os.path.join(merged_root_dir, midi['Genre'])):
+            os.mkdir(os.path.join(merged_root_dir, midi['Genre']))
+        merged_path = os.path.join(merged_root_dir, midi['Genre'] + '/', midi['md5'] + '.mid')
+        merged_multi = get_merged(Multitrack(original_path))
+        merged_multi.write(merged_path)
+
+        score = music21.converter.parse(merged_path)
+        new_score = score.scaleOffsets(changed_rate).scaleDurations(changed_rate)
+        new_score.write('midi', merged_path)
+
+        midi_collection.update_one({'_id': midi['_id']}, {'$set': {'MergedAndScaled': True}})
+
+        print('Progress: {:.2%}\n'.format(midi_collection.count({'MergedAndScaled': True}) / midi_collection.count()))
+
+def change_tempo_in_metadata():
+    test_path = './test4_changed_tempo_dirty.mid'
+    dst_path = './changed_tempo_in_meta.mid'
+    test = mido.MidiFile(test_path)
+    dst_midi = mido.MidiFile()
+
+    changed_rate = get_tempo(test_path)[0] / 120
+    dst_tempo = mido.bpm2tempo(120)
+    for track in test.tracks:
+        new_track = mido.MidiTrack()
+        new_track.name = track.name
+        for msg in track:
+            if msg.is_meta and msg.type == 'set_tempo':
+                msg = mido.MetaMessage('set_tempo', tempo=dst_tempo)
+                new_track.append(msg)
+            else:
+                msg.time = int(msg.time * changed_rate)
+                new_track.append(msg)
+        dst_midi.tracks.append(new_track)
+
+
+    dst_midi.save(dst_path)
+
+    pm = pretty_midi.PrettyMIDI(dst_path)
+    print(get_tempo(test_path), get_tempo(dst_path))
+
+if __name__ == '__main__':
+    merge_then_tempo_unify()
